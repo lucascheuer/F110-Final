3b020971b8dc3d514784b807be479c3c60d42c8e
lucascheuer <scheuer.luca@gmail.com>
2020-04-19 13:58:15 -0400

    Added collision check

--- inc/occgrid.hpp
+++ inc/occgrid.hpp
@@ -10,12 +10,17 @@ class OccGrid
     public:
         OccGrid(int size,float discrete);
         virtual ~OccGrid();
-        void FillOccGrid(const geometry_msgs::Pose &pose_msg,const sensor_msgs::LaserScan::ConstPtr& scan_msg, float obstacle_dilation);
         std::pair<int, int> GridPoint(float x, float y);
+        std::pair<float,float> OccGrid::GetWorldPoint(int row, int col);
         std::pair<float, float> PolarToCartesian(float range, float angle);
         void CartesianToOccupancy(float x, float y);
+        void FillOccGrid(const geometry_msgs::Pose &pose_msg,const sensor_msgs::LaserScan::ConstPtr& scan_msg, float obstacle_dilation);
         bool CartesianInGrid(float x, float y);
-        std::pair<float,float> OccGrid::GetWorldPoint(int row, int col);
+        bool CartesianInGrid(std::pair<float, float> grid_point);
+        bool CheckCollision(float x1, float y1, float x2, float y2);
+        bool CheckCollision(std::pair<float, float> first_point, std::pair<float, float> second_point);
+        
+
 
         // accessors
         int size();
