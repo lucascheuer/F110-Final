b6369f3effb798e62da7c19976b761206203ff00
arnav dhamija <arnav.dhamija@gmail.com>
2020-05-05 13:40:02 -0400

    added commentary

--- inc/pure_pursuit.hpp
+++ inc/pure_pursuit.hpp
@@ -18,14 +18,21 @@ class PurePursuit {
 private:
     vector<State> waypoints_;
     float lookahead_;
+    // Returns distances of waypoints in CMA-ES trajectory relative to
+    // car's position
     vector<float> getWaypointDistances(const geometry_msgs::Pose &pose);
 public:
     PurePursuit(float lookahead);
     ~PurePursuit();
+    // Converts trajectory of State objects to pairs of X,Y coordinates
     vector<pair<float,float>> getPairPoints();
+    // Loads CSV of CMA-ES trajectories
     bool readCMA_ES(string filename);
+    // Checks if path is collision free till lookahead distance
     bool isPathCollisionFree(const geometry_msgs::Pose pose, OccGrid &occ_grid);
+    // Helper function for isPathCollisionFree(..)
     int getClosestIdx(vector<float> &distances, float lookahead);
+    // Finds the closest point in the CMA-ES trajectory
     pair<float,float> findClosest(pair<float,float> &globalPoint);
 };
 #endif
