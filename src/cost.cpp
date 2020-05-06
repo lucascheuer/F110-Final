6ab7d77c4055b3e226dc8affc0b09f74aed51175
lucascheuer <scheuer.luca@gmail.com>
2020-05-05 23:47:48 -0400

    removed unused functions in Cost

--- src/cost.cpp
+++ src/cost.cpp
@@ -5,22 +5,10 @@ Cost::Cost()
 }
 Cost::Cost(Eigen::MatrixXd q, Eigen::MatrixXd r): q_(q), r_(r)
 {
-    // ROS_INFO("cost created");
 }
 
 Cost::~Cost()
 {
-    // ROS_INFO("killing the cost");
-}
-
-void Cost::SetQ(Eigen::MatrixXd q)
-{
-    q_ = q;
-}
-
-void Cost::SetR(Eigen::MatrixXd r)
-{
-    r_ = r;
 }
 
 Eigen::MatrixXd Cost::q()
