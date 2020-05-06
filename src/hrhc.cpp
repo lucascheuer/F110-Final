d484b36da4e0f7bf2549c1e22fa62f6c3b3f4feb
arnav dhamija <arnav.dhamija@gmail.com>
2020-05-06 09:28:08 -0400

    changed fallback v

--- src/hrhc.cpp
+++ src/hrhc.cpp
@@ -89,7 +89,7 @@ Input HRHC::GetNextInput()
     if (inputs_idx_ >= current_inputs_.size())
     {
         // ROS_ERROR("Trajectory complete!");
-        return Input(-1,-0.05);
+        return Input(0.5,-0.05);
     }
     return current_inputs_[inputs_idx_];
 }
